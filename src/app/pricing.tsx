0ddd02b074b19ad3ae286ef872b3ebdf0e09a4a0
crlarrea <crlarrea@protonmail.com>
2025-12-12 22:46:59 +0100

    tweak styling cards

--- src/app/pricing.tsx
+++ src/app/pricing.tsx
@@ -1,19 +1,41 @@
 import styles from "@/app/styles/pricing.module.scss";
+import { RiMoneyPoundCircleLine } from "react-icons/ri";
+import Image from "next/image";
 export default function Pricing() {
   return (
     <section className={styles.pricing}>
-      <article>
-        <span>1 visit</span>
-        <p>£12.50</p>
+      <article className={styles.card}>
+        <div className={styles.description}>
+          <span>1 visit</span>
+          <p>£12.50</p>
+        </div>
+        <div className={styles.wrapper}>
+          <Image src="/dog_2.png" fill alt="dog" />
+        </div>
       </article>
 
-      <article>
-        <span>2 visit</span>
-        <p>£20.00</p>
+      <article className={`${styles.card} ${styles.featured}`}>
+        <div className={styles.description}>
+          <span>2 visits</span>
+          <p>£20.00</p>
+          <div className={styles.bestValue}>
+            <RiMoneyPoundCircleLine /> best value
+          </div>
+        </div>
+        <div className={styles.wrapper}>
+          <Image src="/parrot.png" fill alt="dog" />
+        </div>
       </article>
-      <article>
-        <span>+2 visit</span>
-        <p>get in touch!</p>
+      <article className={styles.card}>
+        <div className={styles.description}>
+          <span>
+            +2 visits <br /> get in touch
+          </span>
+          <p>custom arrangement</p>
+        </div>
+        <div className={styles.wrapper}>
+          <Image src="/cat.png" fill alt="dog" />
+        </div>
       </article>
     </section>
   );
--- src/app/testimonials.tsx
+++ src/app/testimonials.tsx
@@ -8,9 +8,9 @@ export default function Testimonials() {
       </div>
       <span>Kelly Williams Rawlings </span>
       <p>
-        Rebecca is great. reliable, trust worthy, allowing us to leave our cats
-        in the own environment. she is thorough & adores our pets. What more
-        could you need. Will use Rebecca again & again.
+        Rebecca is great - reliable, trust worthy, allowing us to leave our cats
+        in their own environment. She is thorough and adores our pets. What more
+        could you need? Will use Rebecca again and again!
       </p>
     </div>
   );
